bdb8c0663f06bd3279f9d4dc4ff2cf985ae69f2c
Kakunin Konstantin <kakunin@lanit.ru>
2020-10-08 15:23:09 +0300

    NKR-783: проба - успешно докер запустился

--- bin_folder/run_linux_docler_alerts.sh
+++ bin_folder/run_linux_docler_alerts.sh
@@ -4,7 +4,8 @@
 ###
 ### взято от сюда https://github.com/Yelp/elastalert
 ### **********************************************
-CONFIG_DIR=$(pwd)/datana_elastalert
+rm -rf $(pwd)/elastalert
+CONFIG_DIR=$(pwd)/../datana_elastalert
 echo "CONFIG_DIR=$CONFIG_DIR"
 git clone https://github.com/bitsensor/elastalert.git
 cd elastalert
@@ -13,5 +14,4 @@ docker run -d -p 3030:3030 \
   -v $CONFIG_DIR/config/config.json:/opt/elastalert-server/config/config.json \
   -v $CONFIG_DIR/rules:/opt/elastalert/rules \
   -v $CONFIG_DIR/rule_templates:/opt/elastalert/rule_templates \
-  --net="host" \
   --name elastalert bitsensor/elastalert:latest
