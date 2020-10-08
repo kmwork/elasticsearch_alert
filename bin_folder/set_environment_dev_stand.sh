d9f943f7f106a4fd6dd6650ede094e6727a90e32
Kakunin Konstantin <kakunin@lanit.ru>
2020-10-08 19:00:17 +0300

    NKR-783: проба - допилинг v3

--- bin_folder/set_environment_dev_stand.sh
+++ bin_folder/set_environment_dev_stand.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
-export ENV_ES_HOST="datana-logs.datana.ru"
-export ENV_ES_PORT="9200"
+export ES_HOST="datana-logs.datana.ru"
+export ES_PORT="9200"
 
 ### вывод к консоль для отдадки
-echo "[DATANA:SHELL] ENV_ES_HOST = $ENV_ES_HOST"
-echo "[DATANA:SHELL] ENV_ES_PORT = $ENV_ES_PORT"
+echo "[DATANA:SHELL] ES_HOST = $ES_HOST"
+echo "[DATANA:SHELL] ES_PORT = $ES_PORT"
