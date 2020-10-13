7a9ea57faa88233e463e29a0da4e297298ba658c
Kakunin Konstantin <kakunin@lanit.ru>
2020-10-13 21:27:46 +0300

    NKR-813: проба V2-1

--- run_linux_for_localhost.sh
+++ run_linux_for_localhost.sh
@@ -11,5 +11,14 @@ echo "[DATANA:SHELL] ================================ Build dockerfile =========
 ##### docker build --tag $DS_DOCKER_NAME $BIN_DIR
 
 
+### файл - переменные окружения для dev стенда
+ENV_FILE="$BIN_DIR/env_folder/env_dev_stand.env"
+
+### путь на наши конфиги
+CONFIG_DIR=$BIN_DIR/datana_elastalert
+
 echo "[DATANA:SHELL] ================================ Run dockerfile ================================"
-docker run $DS_DOCKER_NAME
+docker run --env-file $ENV_FILE $DS_DOCKER_NAME \
+  -d -p 3030:3030 \
+  -v $CONFIG_DIR/config/elastalert.yaml:/opt/elastalert/config.yaml \
+
