f18bceb9247ffcc0d5eec5db1798abffa6f2cb0a
Kakunin Konstantin <kakunin@lanit.ru>
2020-10-13 22:46:10 +0300

    NKR-813: проба V2-1

--- run_linux_for_localhost.sh
+++ run_linux_for_localhost.sh
@@ -8,10 +8,17 @@ BIN_DIR=$(pwd)
 
 DS_DOCKER_NAME="datana-smart/proba_elastalert:0.0.1"
 echo "[DATANA:SHELL] ================================ Build dockerfile ================================"
-### docker build --tag $DS_DOCKER_NAME $BIN_DIR
+###docker build --tag $DS_DOCKER_NAME $BIN_DIR
 
 ### файл - переменные окружения для dev стенда
 ENV_FILE="$BIN_DIR/env_folder/env_dev_stand.env"
+
+if test -f "$ENV_FILE"; then
+    echo "[DATANA:SHELL] ok, exists file: $ENV_FILE"
+else
+  echo "[DATANA:SHELL] error, not exists file: $ENV_FILE"
+  exit
+fi
 
 ### путь на наши конфиги
 CONFIG_DIR=$BIN_DIR/datana_elastalert
@@ -19,4 +26,5 @@ CONFIG_DIR=$BIN_DIR/datana_elastalert
 echo "[DATANA:SHELL] ================================ Run dockerfile ================================"
 docker run -d -p 3030:3030 \
   -v $CONFIG_DIR/config/elastalert.yaml:/opt/elastalert/config.yaml \
+  --env-file $ENV_FILE \
   $DS_DOCKER_NAME
