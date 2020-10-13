7492a83a1bf92d8a24685801e988da6c11fc4e47
Kakunin Konstantin <kakunin@lanit.ru>
2020-10-13 18:31:07 +0300

    NKR-813: проба V2-1

--- run_linux_for_localhost.sh
+++ run_linux_for_localhost.sh
@@ -8,8 +8,8 @@ BIN_DIR=$(pwd)
 
 DS_DOCKER_NAME="datana-smart/proba_elastalert:0.0.1"
 echo "[DATANA:SHELL] ================================ Build dockerfile ================================"
-docker build --tag $DS_DOCKER_NAME $BIN_DIR
+##### docker build --tag $DS_DOCKER_NAME $BIN_DIR
 
 
 echo "[DATANA:SHELL] ================================ Run dockerfile ================================"
-### docker run   $DS_DOCKER_NAME
+docker run $DS_DOCKER_NAME
