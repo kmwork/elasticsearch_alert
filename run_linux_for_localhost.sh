4127b34d423f64801ec608ef1b02f4ab895569c6
Kakunin Konstantin <kakunin@lanit.ru>
2020-10-13 22:30:51 +0300

    NKR-813: проба V2-1

--- run_linux_for_localhost.sh
+++ run_linux_for_localhost.sh
@@ -13,19 +13,10 @@ echo "[DATANA:SHELL] ================================ Build dockerfile =========
 ### файл - переменные окружения для dev стенда
 ENV_FILE="$BIN_DIR/env_folder/env_dev_stand.env"
 
-if test -f "$ENV_FILE"; then
-    echo "[DATANA:SHELL] ok, exists file: $ENV_FILE"
-else
-  echo "[DATANA:SHELL] error, not exists file: $ENV_FILE"
-  exit
-fi
-#
-
-
 ### путь на наши конфиги
 CONFIG_DIR=$BIN_DIR/datana_elastalert
 
 echo "[DATANA:SHELL] ================================ Run dockerfile ================================"
-printf 'yyy\n\n' | docker run --env-file $ENV_FILE $DS_DOCKER_NAME \
-  -d -p 3030:3030 \
-  -v $CONFIG_DIR/config/elastalert.yaml:/opt/elastalert/config.yaml
+docker run -d -p 3030:3030 \
+  -v $CONFIG_DIR/config/elastalert.yaml:/opt/elastalert/config.yaml \
+  $DS_DOCKER_NAME
