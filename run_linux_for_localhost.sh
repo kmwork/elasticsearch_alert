b617f4e29e993691a810b4ab765e4374b54d351e
Kakunin Konstantin <kakunin@lanit.ru>
2020-10-14 11:41:36 +0300

    NKR-813: проба V2-2

--- run_linux_for_localhost.sh
+++ run_linux_for_localhost.sh
@@ -8,23 +8,30 @@ BIN_DIR=$(pwd)
 
 DS_DOCKER_NAME="datana-smart/proba_elastalert:0.0.1"
 echo "[DATANA:SHELL] ================================ Build dockerfile ================================"
-###docker build --tag $DS_DOCKER_NAME $BIN_DIR
+##### docker build --tag $DS_DOCKER_NAME $BIN_DIR
 
 ### файл - переменные окружения для dev стенда
 ENV_FILE="$BIN_DIR/env_folder/env_dev_stand.env"
 
 if test -f "$ENV_FILE"; then
-    echo "[DATANA:SHELL] ok, exists file: $ENV_FILE"
+  echo "[DATANA:SHELL] ok, exists file: ENV_FILE=$ENV_FILE"
 else
-  echo "[DATANA:SHELL] error, not exists file: $ENV_FILE"
+  echo "[DATANA:SHELL] error, not exists file: ENV_FILE=$ENV_FILE"
   exit
 fi
 
 ### путь на наши конфиги
-CONFIG_DIR=$BIN_DIR/datana_elastalert
+CONFIG_FILE=$BIN_DIR/elastalert_rules_yaml/elastalert_config.yaml
+if test -f "$CONFIG_FILE"; then
+  echo "[DATANA:SHELL] ok, exists file: CONFIG_FILE=$CONFIG_FILE"
+else
+  echo "[DATANA:SHELL] error, not exists file: CONFIG_FILE=$CONFIG_FILE"
+  exit
+fi
 
 echo "[DATANA:SHELL] ================================ Run dockerfile ================================"
-docker run -d -p 3030:3030 \
-  -v $CONFIG_DIR/config/elastalert.yaml:/opt/elastalert/config.yaml \
-  --env-file $ENV_FILE \
-  $DS_DOCKER_NAME
+#docker run -d -p 3030:3030 \
+#  -v $CONFIG_FILE:/opt/elastalert/elastalert_config.yaml \
+#  --env-file $ENV_FILE \
+#  $DS_DOCKER_NAME
+docker run -d -v $CONFIG_FILE:/opt/config/elastalert_config.yaml jertel/elastalert-docker
