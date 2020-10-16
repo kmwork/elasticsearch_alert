ca43dfa8d6665ae4b4e70aa3c8523979a3112b40
Kakunin Konstantin <kakunin@lanit.ru>
2020-10-16 21:35:24 +0300

    NKR-813: наведение красоты

--- dev_run.sh
+++ dev_run.sh
@@ -44,13 +44,7 @@ else
 fi
 
 echo "[DATANA:SHELL] ================================ Run dockerfile ================================"
-#
-#docker run -d -p 3030:3030 \
-#  -v $CONFIG_FILE:/opt/config/elastalert_config.yaml \
-#  --env-file=$ENV_FILE \
-#  $DS_DOCKER_NAME
-
-echo "[DATANA:SHELL] ================================ Build dockerfile ================================"
 docker build --tag $DS_DOCKER_NAME $BIN_DIR
 
+echo "[DATANA:SHELL] ================================ Build dockerfile ================================"
 docker run -d --env-file=$ENV_FILE $DS_DOCKER_NAME
