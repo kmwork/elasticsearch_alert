20cf5c99105822d31e2a782a363c09602b938c0f
Kakunin Konstantin <kakunin@lanit.ru>
2020-10-19 15:38:26 +0300

    NKR-813: code review

--- testing_dev_run.sh
+++ testing_dev_run.sh
@@ -8,44 +8,13 @@ BIN_DIR=$(pwd)
 
 DS_DOCKER_NAME="datana-smart/proba_elastalert:0.0.1"
 
-
-## временнвая папка
-WORK_TEMP_DIR=$BIN_DIR/temp_work
-mkdir -p $WORK_TEMP_DIR
-
-### файл - переменные окружения для dev стенда
-ENV_FILE_TEMPLATE="$BIN_DIR/env_folder/env_dev_stand_with_params.env"
-ENV_FILE="$WORK_TEMP_DIR/.env"
-
-### путь на наши конфиги
-CONFIG_FILE_TEMPLATE=$BIN_DIR/elastalert_rules_yaml/elastalert_config_with_params.yaml
-CONFIG_FILE=$WORK_TEMP_DIR/elastalert_config.yaml
-
-export ES_HOST=datana-logs.datana.ru
-export ES_PORT=9200
-export ES_USERNAME=elastic
-export ES_PASSWORD=changeme
-export ES_USE_SSL=False
-export ES_WRITEBACK_INDEX=dev_index_alert
-export ES_WRITEBACK_ALIAS=dev_alerts
-export RULE_INDEX=datana-smart-dev-logs-*
-export RULE_TELEGRAM_BOT_TOKEN=1171367749:AAFgyOW0LGRLl9NHo9TkZIGNUdd6cFKYGqo
-export RULE_TELEGRAM_ROOM_ID=@DatanaReleaseEvent
-
-TEMPLETE_FILES=$BIN_DIR/elastalert_rules_yaml/*.yaml
-for f in $TEMPLETE_FILES
-do
-  newNameFile="$(basename -- $f)"
-  echo "Processing $f file to $newNameFile"
-  # take action on each file. $f store current file name
-  cat $f | envsubst >$WORK_TEMP_DIR/$newNameFile
-done
-
-
 echo "[DATANA:SHELL] ================================ Run dockerfile ================================"
 docker build --tag $DS_DOCKER_NAME $BIN_DIR
 
 
+
+### файл - переменные окружения для dev стенда
 ENV_FILE=$BIN_DIR/testing_dev.env
+
 echo "[DATANA:SHELL] ================================ Build dockerfile ================================"
 docker run -d --env-file=$ENV_FILE $DS_DOCKER_NAME
